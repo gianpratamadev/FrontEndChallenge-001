ab666b5772288137a6733397e801f908ba8819a4
Gian Pratama <pratama.zidan@gmail.com>
2021-12-29 05:40:04 +0700

    Update navbar html

--- js/init.js
+++ js/init.js
@@ -3,5 +3,6 @@
     $(".sidenav").sidenav();
     $(".parallax").parallax();
     $(".fixed-action-btn").floatingActionButton();
+    $(".dropdown-trigger").dropdown();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
